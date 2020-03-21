2cb9e49177a3a1f4f14b168fada1f18edce66e92
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-03-21 15:10:17 -0400

    Create conditional .js

---
+++ Javascript/conditional .js
@@ -0,0 +1,44 @@
+// const x = 12;
+// const y = 10;
+// if(x===4){
+//     console.log('x is 4.');
+// }else if (x>4){
+//     console.log('x is greater than 4.');
+// }else{
+//     console.log('x is less than 4.')
+// }
+
+// if(x>5||x>10){
+//     console.log('x is more than 5 or y is more than 10.')
+// }
+
+// const color = x >10 ? 'red':'blue';
+// //console.log(color);
+
+// switch(color){
+//     case 'red':
+//         console.log('color is red');
+//         break;
+//     case 'blue':
+//         console.log('color is blue');
+//         break;
+//     default:
+//         console.log('color is unknown');
+//         break;
+// }
+
+// function addNum(num1,num2){
+//    // console.log(num1+num2);
+//    return num1+num2;
+// }
+
+// // addNum(1,5);
+// // addNum(); //NaN: not a number
+// console.log(addNum(1,5));
+
+// const addNums = (num1=3, num2=5) =>{
+//     return num1+num2;
+// }
+
+const addNums = (num1=1, num2=1) => num1+num2;
+console.log(addNums(2,8));
